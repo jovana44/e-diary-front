362a427bb1c5b4fef547c46dcba8fa11f61aba5f
Jovana Vujic <vujicjovana44@gmail.com>
2020-02-18 21:22:19 +0100

    Updates

---
+++ precache-manifest.bae34bd80d51c7d85b69f665f4692e37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9bd196701c985b7213da40f4aebcfe0",
+    "url": "/e-diary-front/static/media/linen-background.c9bd1967.jpg"
+  },
+  {
+    "revision": "1a971e7429e21fc4501c",
+    "url": "/e-diary-front/static/js/runtime~main.2873a134.js"
+  },
+  {
+    "revision": "449fb8ed68bf0d63a28d",
+    "url": "/e-diary-front/static/js/main.3c52f852.chunk.js"
+  },
+  {
+    "revision": "04614b40f34e520b2242",
+    "url": "/e-diary-front/static/js/2.82661491.chunk.js"
+  },
+  {
+    "revision": "449fb8ed68bf0d63a28d",
+    "url": "/e-diary-front/static/css/main.c9fac727.chunk.css"
+  },
+  {
+    "revision": "ea14286b04f4b6ae26f90b989945ec69",
+    "url": "/e-diary-front/index.html"
+  }
+];
